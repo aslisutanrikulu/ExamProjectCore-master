17e2973922dc191c69f80108227bc7506d7948e6
ASLI ŞUTANRIKULU <aslisutanrikulu@hotmail.com>
2022-04-05 15:19:09 +0300

    Add project files.

---
+++ ExamCoreProject.UI/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExamProjectCore.Business.Abstract;
+using ExamProjectCore.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamCoreProject.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IUserService _userService;
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+     
+
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            var login = _userService.GetAll().Where(x => x.UserName == user.UserName && x.Password == user.Password).FirstOrDefault();
+            if (login != null)
+            {           
+                return RedirectToAction("CreateExam", "Exam");
+            }
+            else
+            {
+                ViewBag.Message = "Username or password is wrong.Please try again.";
+                return View();
+            }
+        }
+
+        
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (user != null)
+            {
+                _userService.Create(user);
+                ViewBag.Message = "Success";
+                return View("Login");
+            }
+            else
+            {
+                ViewBag.Message = "Error";
+                return View("Login");
+            }
+
+        }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
---
+++ ExamProjectCore.DataAccess/Concrete/ExamContext.cs
@@ -0,0 +1,21 @@
+﻿using ExamProjectCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamProjectCore.DataAccess.Concrete
+{
+    public class ExamContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=SAMETSUTANRIKUL; database=ExamDb; integrated security=true;");
+        }
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Option> Options { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<CorrectAnswer> CorrectAnswers { get; set; }
+    }
+}
